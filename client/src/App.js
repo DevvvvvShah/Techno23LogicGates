72db8dd03c2254a6428385472b7e599f414d0219
vishal <vckdrt.in@gmail.com>
2023-08-19 22:30:17 +0530

    long-dong

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ function App() {
         <Routes>
           <Route path = "/" element={<Login/>} />
           <Route path = "/instructions" element={<Instruction/>} />
+          <Route path = "/Inter" element={<InteractiveNandBuilder/>} />
 
         </Routes>
       </div>
